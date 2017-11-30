415e268280f88aee5544d5a78dc6ebd632008f1e
Unknown <michael.kahle@yahoo.de>
2017-11-30 16:01:37 +0100

    Debug prints....

--- migrations/m171130_140843_create_user_table.php
+++ migrations/m171130_140843_create_user_table.php
@@ -1,12 +1,17 @@
 <?php
 
+print('loading...');
+
 use yii\db\Schema;
 use yii\db\Migration;
+
+print('used...');
 
 class m171130_140843_create_user_table extends Migration
 {
     public function safeUp()
     {
+        print('up beginning...');
         $this->createTable('user', [
             'id'                   => Schema::TYPE_PK,
             'username'             => Schema::TYPE_STRING . '(25) NOT NULL',
@@ -15,13 +20,18 @@ class m171130_140843_create_user_table extends Migration
             'created_at'           => 'timestamp with time zone NOT NULL DEFAULT now()',
             'updated_at'           => 'timestamp with time zone NOT NULL DEFAULT now()',
         ]);
-
+        print('up running...');
         $this->createIndex('user_unique_username', 'user', 'username', true);
         $this->createIndex('user_unique_email', 'user', 'email', true);
+        print('up finished...');
     }
 
     public function safeDown()
     {
         $this->dropTable('user');
     }
+    
+    
 }
+
+print('loaded...');
