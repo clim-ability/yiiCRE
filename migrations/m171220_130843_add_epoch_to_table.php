42db9e6d6b4cc1fd13702b2d0380a74fa0222d05
Unknown <michael.kahle@yahoo.de>
2017-12-20 14:49:31 +0100

    type mismatch

--- migrations/m171220_130843_add_epoch_to_table.php
+++ migrations/m171220_130843_add_epoch_to_table.php
@@ -2,9 +2,11 @@
 
 use yii\db\Migration;
 
-class m171201_130843_add_epoch_to_table extends Migration {
+class m171220_130843_add_epoch_to_table extends Migration {
 
     public function safeUp() {
+      $this->addEpoch(1970,2000, false);
+      $this->addEpoch(1980,2010, false);
       $this->addEpoch(2021,2050);
       $this->addEpoch(2041,2070);
       $this->addEpoch(2071,2100);
@@ -14,20 +16,22 @@ class m171201_130843_add_epoch_to_table extends Migration {
            $this->remEpoch(2021,2050);
            $this->addEpoch(2041,2070);
            $this->addEpoch(2071,2100);
+           $this->addEpoch(1970,2000);
+           $this->addEpoch(1980,2010);           
     }
     
     private function makeName($begin, $end) {
        return "{$begin}-{$end}"; 
     }
     
-    private function addEpoch($begin, $end) {
+    private function addEpoch($begin, $end, $visible=true) {
         $name = $this->makeName($begin, $end);
         return $this->insert('epoch', [
             'name' => $name,
             'description' => $name,
             'year_begin' => $begin,
             'year_end' => $end,
-            'visible' => true   
+            'visible' => $visible   
         ]);        
     }
     
