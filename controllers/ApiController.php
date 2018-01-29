6b08328c450740fbcb121b27d6dd4603145c2b5b
Unknown <michael.kahle@yahoo.de>
2018-01-29 16:54:33 +0100

    Add Epoch model

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -1,15 +1,13 @@
 <?php
 /**
- * @copyright 2015 University Library of Freiburg
- * @copyright 2015 Leibniz Institute for Regional Geography
- * @copyright 2015 Geographie University of Freiburg
+ * @copyright 2018 Geographie University of Freiburg
  * @licence http://creativecommons.org/licenses/by/4.0/
  */
 namespace app\controllers;
 
 use Yii;
 use yii\web\Controller;
-//use app\models\UserSearch;
+use app\models\Epoch;
 //use yii\web\NotFoundHttpException;
 //use yii\data\ActiveDataProvider;
 //use app\components\RssFormater;
@@ -22,6 +20,27 @@ use yii\web\Controller;
  */
 class ApiController extends Controller
 {
+    public function actionHazards($mode='visible-only') {
+       // returns list of all Hazards.
+       $result = ['value'=>14.5, 'std'=>0.22];
+       \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+       return $result;
+   } 
+    public function actionEpochs($mode='visible-only') {
+       // returns list of all Epochs.
+       $result = ['value'=>14.5, 'std'=>0.22];
+       \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+       return $result;
+   }     
+    public function actionScenarios($mode='visible-only') {
+       // returns list of all Scenarios.
+       $result = ['value'=>14.5, 'std'=>0.22];
+       \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+       return $result;
+   } 
+   
+   
+   
    public function actionHazardValue($latitude, $longitude, $hazard='', $epoch='', $scenario='', $resolution=0.1) {
        
        $result = ['value'=>14.5, 'std'=>0.22];
@@ -34,3 +53,23 @@ class ApiController extends Controller
    
 
 }
+
+
+/**
+ 
+  SELECT value/area as value, std , dlongitude, dlatitude FROM
+(SELECT SUM(ST_Area(ST_Intersection(geom, ellipse))) as area, SUM(cddp*ST_Area(ST_Intersection(geom, ellipse))) AS value, STDDEV(cddp) as std,  
+        regr_slope(cddp, ST_X(ST_Centroid(ST_Transform(geom, 4326)))) as dlongitude, regr_slope(cddp, ST_Y(ST_Centroid(ST_Transform(geom, 4326)))) as dlatitude
+  FROM public.cddp_mean_rcp45_2021_2050_minus_knp,
+(SELECT ST_Buffer(ST_Transform(ST_Translate(ST_SetSRID(ST_MakePoint(0, 0),4326)
+       ,7.85,47.983333), 900915), AVG(sqrt(1.2*ST_Area(ST_SetSRID(geom, 900915))))) AS ellipse
+      FROM public.cddp_mean_rcp45_2021_2050_minus_knp) AS foo 
+  WHERE ST_Intersects(geom, ellipse)) AS sum
+  
+  
+  
+  8:X:Longitude
+  48:Y:Latitude
+  
+ 
+ */
---
+++ models/Epoch.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+class Epoch extends extends ActiveRecord 
+{
+//    public $id;
+//    public $name;
+//    public $description;
+//    public $year_begin;
+//    public $year_end;
+//	  public $visible;
+	
+
+    public static function tableName()
+    {
+        return 'epoch';
+    }
+
+    public function rules()
+    {
+        return [
+            [['name', 'year_begin', , 'year_end'], 'required'],
+            [['visible'], 'boolean'],
+            [['name', 'description'], 'string'],
+            [['year_begin', , 'year_end'], 'integer']
+
+        ];
+    }
+	
+	public function inqAllEpochs() {
+	    $epochs = Epoch::find()
+            ->orderBy(['year_begin', 'year_end'])
+            ->all();
+        return $epochs;
+	}
+	
+	public static function findById($id)
+    {
+        $epoch = Epoch::find()
+            ->where(['id' => $id])
+            ->one();
+        return $epoch;
+    }
+   
+}
