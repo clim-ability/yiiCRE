1d9aa3d40b7d2ecc539d6de9fa15b57a3a14ef9a
Unknown <michael.kahle@yahoo.de>
2019-05-02 14:24:02 +0200

    first table then column

--- migrations/m190502_120124_add_translation_labels_to_tables.php
+++ migrations/m190502_120124_add_translation_labels_to_tables.php
@@ -14,49 +14,49 @@ class m190502_120124_add_translation_labels_to_tables extends Migration
 
     public function safeUp()
     {
-        $this->addColumn("label_en", "hazard", "varchar(255)");
+        $this->addColumn("hazard", "label_en", "varchar(255)");
 		$this->renameColumn("hazard", "decription", "description_en"); 
-        $this->addColumn("label_de", "hazard", "varchar(255)");
-		$this->addColumn("description_de", "hazard", "varchar(4095)");
-        $this->addColumn("label_fr", "hazard", "varchar(255)");
-		$this->addColumn("description_fr", "hazard", "varchar(4095)");	
+        $this->addColumn("hazard", "label_de", "varchar(255)");
+		$this->addColumn("hazard", "description_de", "varchar(4095)");
+        $this->addColumn("hazard", "label_fr", "varchar(255)");
+		$this->addColumn("hazard", "description_fr", "varchar(4095)");	
 
-        $this->addColumn("label_en", "epoch", "varchar(255)");
+        $this->addColumn( "epoch", "label_en","varchar(255)");
 		$this->renameColumn("epoch", "decription", "description_en"); 
-        $this->addColumn("label_de", "epoch", "varchar(255)");
-		$this->addColumn("description_de", "epoch", "varchar(4095)");
-        $this->addColumn("label_fr", "epoch", "varchar(255)");
-		$this->addColumn("description_fr", "epoch", "varchar(4095)");	
+        $this->addColumn( "epoch", "label_de", "varchar(255)");
+		$this->addColumn( "epoch", "description_de", "varchar(4095)");
+        $this->addColumn( "epoch", "label_fr", "varchar(255)");
+		$this->addColumn( "epoch", "description_fr", "varchar(4095)");	
 
-        $this->addColumn("label_en", "scenario", "varchar(255)");
+        $this->addColumn("scenario", "label_en", "varchar(255)");
 		$this->renameColumn("scenario", "decription", "description_en"); 
-        $this->addColumn("label_de", "scenario", "varchar(255)");
-		$this->addColumn("description_de", "scenario", "varchar(4095)");
-        $this->addColumn("label_fr", "scenario", "varchar(255)");
-		$this->addColumn("description_fr", "scenario", "varchar(4095)");			
+        $this->addColumn("scenario", "label_de", "varchar(255)");
+		$this->addColumn("scenario", "description_de", "varchar(4095)");
+        $this->addColumn("scenario", "label_fr", "varchar(255)");
+		$this->addColumn("scenario", "description_fr", "varchar(4095)");			
     }
 
     public function safeDown()
     {
-        $this->dropColumn("label_en", "hazard");
+        $this->dropColumn("hazard", "label_en");
 		$this->renameColumn("hazard", "decription_en", "description");
-        $this->dropColumn("label_de", "hazard");
-        $this->dropColumn("description_de", "hazard");
-        $this->dropColumn("label_fr", "hazard");
-        $this->dropColumn("description_fr", "hazard");		
+        $this->dropColumn("hazard", "label_de");
+        $this->dropColumn("hazard", "description_de");
+        $this->dropColumn("hazard", "label_fr");
+        $this->dropColumn("hazard", "description_fr");		
 		
-        $this->dropColumn("label_en", "epoch");
+        $this->dropColumn("epoch", "label_en");
 		$this->renameColumn("epoch", "decription_en", "description");
-        $this->dropColumn("label_de", "epoch");
-        $this->dropColumn("description_de", "epoch");
-        $this->dropColumn("label_fr", "epoch");
-        $this->dropColumn("description_fr", "epoch");	
+        $this->dropColumn("epoch", "label_de");
+        $this->dropColumn("epoch", "description_de");
+        $this->dropColumn("epoch", "label_fr");
+        $this->dropColumn("epoch", "description_fr");	
 
-        $this->dropColumn("label_en", "scenario");
+        $this->dropColumn("epoch", "label_en");
 		$this->renameColumn("scenario", "decription_en", "description");
-        $this->dropColumn("label_de", "scenario");
-        $this->dropColumn("description_de", "scenario");
-        $this->dropColumn("label_fr", "scenario");
-        $this->dropColumn("description_fr", "scenario");			
+        $this->dropColumn("epoch", "label_de");
+        $this->dropColumn("epoch", "description_de");
+        $this->dropColumn("epoch", "label_fr");
+        $this->dropColumn("epoch", "description_fr");			
     }
 }
