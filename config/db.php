5caeb8ef50e4d5fdea49fc1e8178a6ee550e4546
Unknown <michael.kahle@yahoo.de>
2019-05-02 14:13:26 +0200

    Define db for migration

--- config/db.php
+++ config/db.php
@@ -1,10 +1,19 @@
 <?php
+require_once __DIR__ . '/../../db-config.php';
+
+foreach(inqDbConnections() as $dbkey=>$dbdata) {
+  if ('pgsql:cre' == $dbkey ) {	
+    define('MIGRATION_CONFIG_DB_USER', $dbdata['user']);
+    define('MIGRATION_CONFIG_DB_PASSWORD', $dbdata['password']);
+    define('MIGRATION_CONFIG_DB_CONNECTION', $dbdata['pdo']);	
+  }	
+}
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => LOCAL_CONFIG_DB_CONNECTION,
-    'username' => LOCAL_CONFIG_DB_USER,
-    'password' => LOCAL_CONFIG_DB_PASSWORD,
+    'dsn' => MIGRATION_CONFIG_DB_CONNECTION,
+    'username' => MIGRATION_CONFIG_DB_USER,
+    'password' => MIGRATION_CONFIG_DB_PASSWORD,
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
