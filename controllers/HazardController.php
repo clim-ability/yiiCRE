fee013482a37f1e7ef5234599e77ec7733bc5b05
Unknown <michael.kahle@yahoo.de>
2019-05-02 14:54:06 +0200

    Add Hazard Controller

---
+++ controllers/HazardController.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @copyright 2015 University Library of Freiburg
+ * @copyright 2015 Leibniz Institute for Regional Geography
+ * @copyright 2015 Geographie University of Freiburg
+ * @licence http://creativecommons.org/licenses/by/4.0/
+ */
+namespace app\controllers;
+
+use Yii;
+use app\models\Hazard;
+use app\controllers\ControllerBase;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+
+/**
+ * Class QuoteController
+ * @package app\modules\grouping\controllers
+ * @since 2.0
+ */
+class QuoteController extends ControllerBase
+{
+
+
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => array_merge(
+                    [
+                        'remove' => ['post', 'delete'],
+                    ], $this->crudVerbs()
+                ),
+            ],
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => array_merge(
+                    [
+                        [
+                            'allow' => true,
+                            'actions' => ['list', 'show', 'new' ,'modify', 'remove'],
+                            'roles' => ['@','sysadmin','admin'],
+                        ],
+                    ], $this->crudRules()),
+            ],
+        ];
+    }
+
+    protected function findOneModel($id)
+    {
+        return Quote::findOne($id);
+    }
+
+    protected function newModel()
+    {
+        return new Quote();
+    }
+
+    protected function newSearchModel()
+    {
+        return new QuoteSearch();
+    }
+
+    protected function getColumns()
+    {
+        return array_keys($this->getAllColumns());
+    }
+
+    protected function getMessages()
+    {
+        return [
+            'title' => 'Hazards', // Yii::t('p:quote', 'Quotes'),
+            'item' => 'Hazard', // Yii::t('p:quote', 'Quote'),
+            'create' => 'Create Hazard', // Yii::t('p:quote', 'Create Quote'),
+        ];
+    }
+
+}
