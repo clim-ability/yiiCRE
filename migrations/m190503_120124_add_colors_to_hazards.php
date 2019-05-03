0c28c637acf1f4073b31dda994441b715f5c2b7d
Unknown <michael.kahle@yahoo.de>
2019-05-03 19:31:24 +0200

    update values

--- migrations/m190503_120124_add_colors_to_hazards.php
+++ migrations/m190503_120124_add_colors_to_hazards.php
@@ -16,15 +16,15 @@ class m190503_120124_add_colors_to_hazards extends Migration
     {
         $this->addColumn("hazard", "color_min", "varchar(15)"); 
 		$this->addColumn("hazard", "color_max", "varchar(15)");
-		$this->update("hazard", ["color_min", "color_max"], "name='cddp'", ["#6dce70","#724702"]);
-        $this->update("hazard", ["color_min", "color_max"], "name='hd'", ["#f2a9ad","#ea0914"]);	
-        $this->update("hazard", ["color_min", "color_max"], "name='fd'", ["#b7e3e8","#09d7ea"]);	
-        $this->update("hazard", ["color_min", "color_max"], "name='tr'", ["#f2a9ad","#ea0914"]);	
-        $this->update("hazard", ["color_min", "color_max"], "name='rr20'", ["#d9d9f4","#0202f4"]);	
-        $this->update("hazard", ["color_min", "color_max"], "name='rr_winter'", ["#e3d7f4F","#7e29f4"]);	
-        $this->update("hazard", ["color_min", "color_max"], "name='rr_summer'", ["#dcf9db","#06b203"]);	
-        $this->update("hazard", ["color_min", "color_max"], "name='hq'", ["#f9d1dd","#d80444"]);	
-        $this->update("hazard", ["color_min", "color_max"], "name='height'", ["#cae03e","#f2cd8a"]);		
+		$this->update("hazard", ["color_min" => "#6dce70", "color_max" => "#724702"], "name='cddp'");
+        $this->update("hazard", ["color_min" => "#f2a9ad", "color_max" => "#ea0914"], "name='hd'");	
+        $this->update("hazard", ["color_min" => "#b7e3e8", "color_max" => "#09d7ea"], "name='fd'");	
+        $this->update("hazard", ["color_min" => "#f2a9ad", "color_max" => "#ea0914"], "name='tr'");	
+        $this->update("hazard", ["color_min" => "#d9d9f4", "color_max" => "#0202f4"], "name='rr20'");	
+        $this->update("hazard", ["color_min" => "#e3d7f4", "color_max" => "#7e29f4"], "name='rr_winter'");	
+        $this->update("hazard", ["color_min" => "#dcf9db", "color_max" => "#06b203"], "name='rr_summer'");	
+        $this->update("hazard", ["color_min" => "#f9d1dd", "color_max" => "#d80444"], "name='hq'");	
+        $this->update("hazard", ["color_min" => "#cae03e", "color_max" => "#f2cd8a"], "name='height'");		
     }
 
     public function safeDown()
