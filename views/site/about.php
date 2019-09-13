7724c6c5d3ae9c8df98ab66d012170d4dfd7314a
Unknown <michael.kahle@yahoo.de>
2019-09-13 15:46:29 +0200

    Try translation

--- views/site/about.php
+++ views/site/about.php
@@ -3,9 +3,14 @@
 /* @var $this yii\web\View */
 
 use yii\helpers\Html;
+use app\modules\translation\widgets\LanguageTranslate;
 
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
+
+function tr($c, $m, $p = []) {
+    echo LanguageTranslate::widget(['category' => $c, 'message' => $m, 'params' => $p]);
+}
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
@@ -13,6 +18,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>
         This is the About page. You may modify the following file to customize its content:
     </p>
+	<p><?php tr('sugg:about', 'Hello'); ?> 
 
     <code><?= __FILE__ ?></code>
 </div>
