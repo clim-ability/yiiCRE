da4f8b8777bce6d07543407656daf8cab5ff161f
Unknown <michael.kahle@yahoo.de>
2019-09-19 11:27:06 +0200

    Translation of table content

--- views/base/translate.php
+++ views/base/translate.php
@@ -1,6 +1,11 @@
 <?php
 
 use yii\helpers\Html;
+use app\modules\translation\widgets\LanguageTranslate;
+
+function tr($c, $m, $p = []) {
+    echo LanguageTranslate::widget(['category' => $c, 'message' => $m, 'params' => $p]);
+}
 
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -9,15 +14,39 @@ $this->title = $messages['title'];
 ?>
 <div class="item-index">
 
+<ul>
 <?php
 foreach($dataProvider->getModels() as $data) {
-	var_dump($data); 
+  echo '<li><ul>';	
+  $name = $data->formName();
+  if (in_array('name', $columns) && in_array('description', $columns)) {
+	echo '<li>';
+	tr($name.':name', $data['name']); 
+	echo '</li><li>';
+	tr('hazard:description', $data['description']); 
+    echo '</li>';
+  } elseif (in_array('name', $columns))	{
+	echo '<li>';
+    tr($name.':name', $data['name']); 
+	echo '</li><li>';	
+	 tr($name.':description', $data['name']); 
+    echo '</li>';
+  } elseif (in_array('description', $columns)) {
+	echo '<li>';	  
+	      tr($name.':name', $data['description']); 
+	echo '</li><li>';		  
+	 tr($name.':description', $data['description']);
+    echo '</li>';
+  }	elseif (in_array('label', $columns)) {
+	echo '<li>';	  
+	tr($name.':name', $data['label']); 
+	echo '</li><li>';		   
+	tr($name.':description', $data['label']); 
+    echo '</li>';
+  }	  
+  echo '</ul></li>';	
 }
-foreach($columns as $column) {
-	var_dump($column); 
-}
-
-var_dump($columns);
+</ul>
 ?>
 
 </div>
