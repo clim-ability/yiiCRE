7a5624164e0cdd6bf50922bf9a26f537b718881d
Unknown <michael.kahle@yahoo.de>
2019-09-23 16:15:33 +0200

    Round

--- views/site/index.php
+++ views/site/index.php
@@ -90,7 +90,7 @@ function tr($c, $m, $p = []) {
 	   </tr>
 		  <tr>   
 	   <td><?php tr('main', 'Nearest Station'); ?>:</td><td>{{nearestStation.name}} ({{nearestStation.abbreviation}})</td>
-	   <td><?php tr('main', 'Elevation'); ?>:</td><td>{{nearestStation.elevation}} m</td><td>Distance: {{Math.round(nearestStation.elevation/1000)}} km</td>
+	   <td><?php tr('main', 'Elevation'); ?>:</td><td>{{nearestStation.elevation}} m</td><td>Distance: {{Math.round(parseFloat(nearestStation.elevation/1000)).toString()}} km</td>
 	  </tr>	
 	 </table>	
 	
