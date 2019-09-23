b7fb17d56c5c25b8c3da5f1cf440de4cd8c0193d
Unknown <michael.kahle@yahoo.de>
2019-09-23 11:44:29 +0200

    set limit 9999

--- controllers/ControllerBase.php
+++ controllers/ControllerBase.php
@@ -211,7 +211,7 @@ class ControllerBase extends Controller
         $columns = $this->getColumns();
         if ($searchModel !== null) {
             $dataProvider = $searchModel->search([]);
-			$dataProvider->setPagination(['defaultPageSize'=>-1]);
+			$dataProvider->setPagination(['defaultPageSize'=>9999]);
             return $this->render('@app/views/base/translate', [
                 'columns' => $columns,
                 'messages' => $this->getMessages(),
