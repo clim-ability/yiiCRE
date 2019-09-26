82302cf972a5fca1461d9536fb515b9c57cd2126
Unknown <michael.kahle@yahoo.de>
2019-09-26 10:37:12 +0200

    Adapt river values

--- models/Gis.php
+++ models/Gis.php
@@ -178,8 +178,8 @@ class Gis extends ActiveRecord
 	  $result['const'] = ['abs_pos' => 1.0, 'abs_neg' => 1.0, 'rel_pos' => 1.0,'rel_neg' => 1.0];
       $river = Gis::getDistanceToRiver($latitude, $longitude);	  
 	  $riverRel = $river['catchment']/($river['distance']+1.0);	
-      $riverAbs = $river['length']/($river['distance']+1.0);		  
-	  $result['hq'] = ['abs_pos' => $riverAbs, 'abs_neg' => (200.0+$river['distance'])/100.0, 'rel_pos' => $riverRel,'rel_neg' => $river['distance']/100.0];
+      $riverAbs = $river['length']/($river['distance']+100.0)/10;		  
+	  $result['hq'] = ['abs_pos' => $riverAbs, 'abs_neg' => (200.0+$river['distance'])/1000.0, 'rel_pos' => $riverRel,'rel_neg' => $river['distance']/100.0];
 	  /// add city
       return $result;	  
 	}
