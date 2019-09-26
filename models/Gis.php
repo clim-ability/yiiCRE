79c391c0ed292ddd887952be54afc474f21bc9a0
Unknown <michael.kahle@yahoo.de>
2019-09-26 17:18:58 +0200

    remove dump

--- models/Gis.php
+++ models/Gis.php
@@ -253,7 +253,7 @@ class Gis extends ActiveRecord
 				  $sql2 .= ','.$key.'='.($factors[$key]*$corrFactors[$key]+$corrOffsets[$key]).' ';
 			   }
 			   $sql2 .= 'WHERE hazard_id = '.$hazardId.' AND danger_id = '.$danger['id'];
-			   var_dump($sql2);
+			   //var_dump($sql2);
                $command2 = $connection->createCommand($sql2);	
 			   $command2->execute();
 		    }
