5e1c1f5fd822944738b4a0ecadfe9e76e5dd69b5
Unknown <michael.kahle@yahoo.de>
2019-11-22 16:36:35 +0100

    Add search

--- models/Scenario.php
+++ models/Scenario.php
@@ -80,5 +80,26 @@ class Scenario extends ActiveRecord
 		}
 		return $scenario;
 	}	
+	
+public function search($params)
+    {
+        $query = Scenario::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        return $dataProvider;
+    }
+	
+	
    
 }
