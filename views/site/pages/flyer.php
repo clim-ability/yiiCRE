fd5005a118a50b2f6a3604fcb5f5c12ddfe65f0b
Unknown <michael.kahle@yahoo.de>
2020-02-14 14:42:05 +0100

    translate function

--- views/site/pages/flyer.php
+++ views/site/pages/flyer.php
@@ -3,6 +3,10 @@ use app\widgets\TmbCards;
 use app\utils\FlyerItems;
 use app\modules\translation\widgets\LanguageTranslate;
 
+function tr($c, $m, $p = []) {
+    echo LanguageTranslate::widget(['category' => $c, 'message' => $m, 'params' => $p]);
+}
+
 $this->title = Yii::t('p:flyer', 'Flyer');
 $items = FlyerItems::getItems();
 ?>
