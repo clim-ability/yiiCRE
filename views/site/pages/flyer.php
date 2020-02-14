1738e7f67baddbf26c67b2b24b7d43a42b9493c3
Unknown <michael.kahle@yahoo.de>
2020-02-14 14:39:09 +0100

    headline

--- views/site/pages/flyer.php
+++ views/site/pages/flyer.php
@@ -4,6 +4,8 @@ use app\utils\FlyerItems;
 
 $this->title = Yii::t('p:flyer', 'Flyer');
 $items = FlyerItems::getItems();
+?>
+<h2><?php tr('p:flyer', 'Zusätzliche Informationen'); ?></h2>
+<?php
 echo TmbCards::widget(['items' => $items]);
-
 ?>
