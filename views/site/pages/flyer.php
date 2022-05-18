05e2fbf719201f0f53a8b7ae12bf39b0ce29a651
KMicha <michael.kahle@yahoo.de>
2022-05-18 11:28:57 +0200

    flyer -> glass

--- views/site/pages/flyer.php
+++ views/site/pages/flyer.php
@@ -10,7 +10,17 @@ function tr($c, $m, $p = []) {
 $this->title = Yii::t('p:flyer', 'Flyer');
 $items = FlyerItems::getItems();
 ?>
+
+<style>
+ body {
+   background-image: url('/images/wolke5.jpg') !important; 
+ }
+</style>
+
 <h2><?php tr('p:flyer', 'Zusätzliche Informationen'); ?></h2>
+
+<div class='gxxlass70'>
 <?php
 echo TmbCards::widget(['items' => $items]);
 ?>
+</div>
