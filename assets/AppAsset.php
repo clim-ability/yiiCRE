e2455a3401e7e4ce95ece2e41e49cc3efede4710
KMicha <michael.kahle@yahoo.de>
2022-05-18 11:24:55 +0200

    load glass.css

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,6 +21,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/glass.css',
     ];
     public $js = [
         'js/site.js',	
